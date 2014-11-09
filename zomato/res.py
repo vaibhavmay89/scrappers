45d87b79f112860041cf8d8351d576d51d2983dc
vaibhavmay89 <vaibhavmay89@gmail.com>
2014-11-09 18:11:46 +0530

    scrapper done

--- zomato/res.py
+++ zomato/res.py
@@ -9,11 +9,11 @@ urls = f.readlines()
 
 
 dict = {
-	'name': '//h1/a/span',
+	'name': '//h1/a/span/text()',
 	'time': "//span[@class='res-info-timings']/div/div/div/span/text()",
 	'cost': '//span[@itemprop="priceRange"]/text()',
 	'phone' : "//div[@id='phoneNoString']/div/span/span/span/text()",
-	'highlight': "//div[@class='res-info-feature-text']",
+	'highlight': "//div[@class='res-info-feature-text']/text()",
 	"locality":'//span[@itemprop="addressLocality"]/text()',
 	"address": '//h2[@itemprop="address"]/text()',
 	"ratings":"//div[@class='rating-for-2331 rating-div small-rating rrw-aggregate level-6']/text()",
@@ -21,10 +21,20 @@ dict = {
 	"noReviews": "//a[@class='default-section-title everyone empty']/span/text()"
 }
 
-print(dict['name'])
 
+f = open('full_list.txt','a')
 
-for i in range(0,10):#int(len(urls)/100)): 
+l = []
+for i in range(0,int(len(urls)): 
 	urls[i] = urls[i].replace("\n","")
 	page  = requests.get(urls[i])
 	tree = html.fromstring(page.text)
+	for key in dict: 
+		##print(key)
+		l.append(tree.xpath(dict[key]))
+	print(str(l))
+	f.write("'"+str(i)+"',"+str(l)+'\n||||')
+	l = []
+	print (i)
+
+
